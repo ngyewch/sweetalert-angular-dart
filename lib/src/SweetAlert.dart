8adc636852a64441f85bb745df64cbde50aaa7b7
Nick Ng <ngyewch@gmail.com>
2016-11-16 00:11:54 +0800

    Implementation.

--- lib/src/SweetAlert.dart
+++ lib/src/SweetAlert.dart
@@ -8,20 +8,65 @@ import 'dart:js' as js;
 
 class SweetAlert {
 
-  static void swal(String title, {String text, String type, bool showCancelButton, String confirmButtonColor, String confirmButtonText, bool closeOnConfirm, String imageUrl}) {
-    Map<String, dynamic> options = new Map<String, dynamic>();
-    options['title'] = 'Hello';
-    options['text'] = 'World';
+  static void swal(String title,
+      {String text, String type, bool showConfirmButton, bool showCancelButton, String confirmButtonColor, String confirmButtonText, String cancelButtonColor, String cancelButtonText, bool closeOnConfirm, bool closeOnCancel, String imageUrl, bool html, int timer, String animation, String inputPlaceholder, bool showLoaderOnConfirm, Function callback}) {
+    var options = {
+      'title': title
+    };
+    if (text != null) {
+      options['text'] = text;
+    }
+    if (type != null) {
+      options['type'] = type;
+    }
+    if (showConfirmButton != null) {
+      options['showConfirmButton'] = showConfirmButton;
+    }
+    if (showCancelButton != null) {
+      options['showCancelButton'] = showCancelButton;
+    }
+    if (confirmButtonColor != null) {
+      options['confirmButtonColor'] = confirmButtonColor;
+    }
+    if (confirmButtonText != null) {
+      options['confirmButtonText'] = confirmButtonText;
+    }
+    if (cancelButtonColor != null) {
+      options['cancelButtonColor'] = cancelButtonColor;
+    }
+    if (cancelButtonText != null) {
+      options['cancelButtonText'] = cancelButtonText;
+    }
+    if (closeOnConfirm != null) {
+      options['closeOnConfirm'] = closeOnConfirm;
+    }
+    if (closeOnCancel != null) {
+      options['closeOnCancel'] = closeOnCancel;
+    }
+    if (imageUrl != null) {
+      options['imageUrl'] = imageUrl;
+    }
+    if (html != null) {
+      options['html'] = html;
+    }
+    if (timer != null) {
+      options['timer'] = timer;
+    }
+    if (animation != null) {
+      options['animation'] = animation;
+    }
+    if (inputPlaceholder != null) {
+      options['inputPlaceholder'] = inputPlaceholder;
+    }
+    if (showLoaderOnConfirm != null) {
+      options['showLoaderOnConfirm'] = showLoaderOnConfirm;
+    }
 
-    js.context.callMethod("swal", [new js.JsObject.jsify({
-      'title': title,
-      'text': text,
-      'type': type,
-      'showCancelButton': showCancelButton,
-      'confirmButtonColor': confirmButtonColor,
-      'confirmButtonText': confirmButtonText,
-      'closeOnConfirm': closeOnConfirm,
-      'imageUrl': imageUrl
-    })]);
+    var args = [];
+    args.add(new js.JsObject.jsify(options));
+    if (callback != null) {
+      args.add(callback);
+    }
+    js.context.callMethod("swal", args);
   }
 }
--- web/AppComponent.dart
+++ web/AppComponent.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:angular2/core.dart';
 
 import 'package:sweetalert_angular_dart/sweetalert_angular_dart.dart';
@@ -13,25 +15,86 @@ class AppComponent {
   }
 
   void example2() {
-    SweetAlert.swal("Here's a message!", text: "It's pretty, isn't it?");
+    SweetAlert.swal("Here's a message!",
+        text: "It's pretty, isn't it?");
   }
 
   void example3() {
-    SweetAlert.swal("Good job!", text: "You clicked the button!", type: "success");
+    SweetAlert.swal("Good job!",
+        text: "You clicked the button!",
+        type: "success");
   }
 
   void example4() {
-    //
+    SweetAlert.swal("Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        closeOnConfirm: false,
+        callback: (bool isConfirm) =>
+            SweetAlert.swal("Deleted!",
+                text: "Your imaginary file has been deleted.",
+                type: "success"));
   }
 
   void example5() {
-    //
+    SweetAlert.swal("Are you sure?",
+        text: "You will not be able to recover this imaginary file!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel plx!",
+        closeOnConfirm: false,
+        closeOnCancel: false,
+        callback: (bool isConfirm) => isConfirm
+            ? SweetAlert.swal("Deleted!", text: "Your imaginary file has been deleted.", type: "success")
+            : SweetAlert.swal("Cancelled", text: "Your imaginary file is safe :)", type: "error"));
   }
 
   void example6() {
-    SweetAlert.swal(
-      "Sweet!",
-      text: "Here's a custom image.",
+    SweetAlert.swal("Sweet!",
+        text: "Here's a custom image.",
         imageUrl: "images/thumbs-up.jpg");
+  }
+
+  void example7() {
+    SweetAlert.swal("HTML <small>Title</small>!",
+      text: 'A custom <span style="color:#F8BB86">html<span> message.',
+      html: true);
+  }
+
+  void example8() {
+    SweetAlert.swal("Auto close alert!",
+      text: "I will close in 2 seconds.",
+      timer: 2000,
+      showConfirmButton: false);
+  }
+
+  void example9() {
+    SweetAlert.swal("An input!",
+      text: "Write something interesting:",
+      type: "input",
+      showCancelButton: true,
+      closeOnConfirm: false,
+      animation: "slide-from-top",
+      inputPlaceholder: "Write something",
+      callback: (inputValue) => (inputValue is bool)
+          ? inputValue
+          : ((inputValue != "")
+              ? SweetAlert.swal("Nice!", text: "You wrote: " + inputValue, type: "success")
+              : false));
+  }
+
+  void example10() {
+    SweetAlert.swal("Ajax request example",
+      text: "Submit to run ajax request",
+      type: "info",
+      showCancelButton: true,
+      closeOnConfirm: false,
+      showLoaderOnConfirm: true,
+      callback: (bool isConfirm) => new Timer(const Duration(milliseconds: 2000), () => SweetAlert.swal("Ajax request finished!")));
   }
 }
