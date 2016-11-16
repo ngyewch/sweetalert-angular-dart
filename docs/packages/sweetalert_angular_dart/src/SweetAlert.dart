fc460a191b2eda4be4684be0be374caeccaed11a
Nick Ng <ngyewch@gmail.com>
2016-11-16 23:44:42 +0800

    Added missing doc files.

---
+++ docs/packages/sweetalert_angular_dart/src/SweetAlert.dart
@@ -0,0 +1,215 @@
+// Copyright (c) 2016, Nick Ng. All rights reserved. Use of this source code
+
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+import 'package:angular2/core.dart';
+
+import 'dart:js' as js;
+
+class SweetAlert {
+
+  static void swal(String title,
+      {
+      String text,
+      String type,
+      bool allowEscapeKey,
+      String customClass,
+      bool allowOutsideClick,
+      bool showCancelButton,
+      bool showConfirmButton,
+      String confirmButtonText,
+      String confirmButtonColor,
+      String cancelButtonText,
+      bool closeOnConfirm,
+      bool closeOnCancel,
+      String imageUrl,
+      String imageSize,
+      int timer,
+      bool html,
+      String animation,
+      String inputType,
+      String inputPlaceholder,
+      String inputValue,
+      bool showLoaderOnConfirm,
+      Function callback}) {
+    var options = { 'title': title};
+    if (text != null) {
+      options['text'] = text;
+    }
+    if (type != null) {
+      options['type'] = type;
+    }
+    if (allowEscapeKey != null) {
+      options['allowEscapeKey'] = allowEscapeKey;
+    }
+    if (customClass != null) {
+      options['customClass'] = customClass;
+    }
+    if (allowOutsideClick != null) {
+      options['allowOutsideClick'] = allowOutsideClick;
+    }
+    if (showCancelButton != null) {
+      options['showCancelButton'] = showCancelButton;
+    }
+    if (showConfirmButton != null) {
+      options['showConfirmButton'] = showConfirmButton;
+    }
+    if (confirmButtonText != null) {
+      options['confirmButtonText'] = confirmButtonText;
+    }
+    if (confirmButtonColor != null) {
+      options['confirmButtonColor'] = confirmButtonColor;
+    }
+    if (cancelButtonText != null) {
+      options['cancelButtonText'] = cancelButtonText;
+    }
+    if (closeOnConfirm != null) {
+      options['closeOnConfirm'] = closeOnConfirm;
+    }
+    if (closeOnCancel != null) {
+      options['closeOnCancel'] = closeOnCancel;
+    }
+    if (imageUrl != null) {
+      options['imageUrl'] = imageUrl;
+    }
+    if (imageSize != null) {
+      options['imageSize'] = imageSize;
+    }
+    if (timer != null) {
+      options['timer'] = timer;
+    }
+    if (html != null) {
+      options['html'] = html;
+    }
+    if (animation != null) {
+      options['animation'] = animation;
+    }
+    if (inputType != null) {
+      options['inputType'] = inputType;
+    }
+    if (inputPlaceholder != null) {
+      options['inputPlaceholder'] = inputPlaceholder;
+    }
+    if (inputValue != null) {
+      options['inputValue'] = inputValue;
+    }
+    if (showLoaderOnConfirm != null) {
+      options['showLoaderOnConfirm'] = showLoaderOnConfirm;
+    }
+
+    var args = [];
+    args.add(new js.JsObject.jsify(options));
+    if (callback is Function) {
+      args.add(callback);
+    }
+    js.context.callMethod("swal", args);
+  }
+
+  static void setDefaults({
+  String text,
+  String type,
+  bool allowEscapeKey,
+  String customClass,
+  bool allowOutsideClick,
+  bool showCancelButton,
+  bool showConfirmButton,
+  String confirmButtonText,
+  String confirmButtonColor,
+  String cancelButtonText,
+  bool closeOnConfirm,
+  bool closeOnCancel,
+  String imageUrl,
+  String imageSize,
+  int timer,
+  bool html,
+  String animation,
+  String inputType,
+  String inputPlaceholder,
+  String inputValue,
+  bool showLoaderOnConfirm
+  }) {
+    var options = {};
+    if (text != null) {
+      options['text'] = text;
+    }
+    if (type != null) {
+      options['type'] = type;
+    }
+    if (allowEscapeKey != null) {
+      options['allowEscapeKey'] = allowEscapeKey;
+    }
+    if (customClass != null) {
+      options['customClass'] = customClass;
+    }
+    if (allowOutsideClick != null) {
+      options['allowOutsideClick'] = allowOutsideClick;
+    }
+    if (showCancelButton != null) {
+      options['showCancelButton'] = showCancelButton;
+    }
+    if (showConfirmButton != null) {
+      options['showConfirmButton'] = showConfirmButton;
+    }
+    if (confirmButtonText != null) {
+      options['confirmButtonText'] = confirmButtonText;
+    }
+    if (confirmButtonColor != null) {
+      options['confirmButtonColor'] = confirmButtonColor;
+    }
+    if (cancelButtonText != null) {
+      options['cancelButtonText'] = cancelButtonText;
+    }
+    if (closeOnConfirm != null) {
+      options['closeOnConfirm'] = closeOnConfirm;
+    }
+    if (closeOnCancel != null) {
+      options['closeOnCancel'] = closeOnCancel;
+    }
+    if (imageUrl != null) {
+      options['imageUrl'] = imageUrl;
+    }
+    if (imageSize != null) {
+      options['imageSize'] = imageSize;
+    }
+    if (timer != null) {
+      options['timer'] = timer;
+    }
+    if (html != null) {
+      options['html'] = html;
+    }
+    if (animation != null) {
+      options['animation'] = animation;
+    }
+    if (inputType != null) {
+      options['inputType'] = inputType;
+    }
+    if (inputPlaceholder != null) {
+      options['inputPlaceholder'] = inputPlaceholder;
+    }
+    if (inputValue != null) {
+      options['inputValue'] = inputValue;
+    }
+    if (showLoaderOnConfirm != null) {
+      options['showLoaderOnConfirm'] = showLoaderOnConfirm;
+    }
+
+    js.context["swal"].callMethod(
+        "setDefaults", [new js.JsObject.jsify(options)]);
+  }
+
+  static void close() {
+    js.context["swal"].callMethod("close", []);
+  }
+
+  static void showInputError(String error) {
+    js.context["swal"].callMethod("showInputError", [error]);
+  }
+
+  static void enableButtons() {
+    js.context["swal"].callMethod("enableButtons", []);
+  }
+
+  static void disableButtons(String error) {
+    js.context["swal"].callMethod("disableButtons", []);
+  }
+}
---
+++ docs/packages/sweetalert_angular_dart/sweetalert_angular_dart.template.dart
@@ -0,0 +1,24 @@
+// @ignoreProblemForFile annotate_overrides
+// @ignoreProblemForFile cancel_subscriptions
+// @ignoreProblemForFile constant_identifier_names
+// @ignoreProblemForFile non_constant_identifier_names
+// @ignoreProblemForFile implementation_imports
+// @ignoreProblemForFile library_prefixes
+// @ignoreProblemForFile type_annotate_public_apis
+// @ignoreProblemForFile STRONG_MODE_DOWN_CAST_COMPOSITE
+// @ignoreProblemForFile UNUSED_IMPORT
+// @ignoreProblemForFile UNUSED_SHOWN_NAME
+// @ignoreProblemForFile UNUSED_LOCAL_VARIABLE
+library sweetalert_angular_dart.template.dart;
+
+import 'sweetalert_angular_dart.dart';
+import 'src/SweetAlert.dart' show SweetAlert;
+import 'src/SweetAlert.template.dart' as i0;
+export 'sweetalert_angular_dart.dart';
+export 'src/SweetAlert.dart';
+
+var _visited = false;
+void initReflector() {
+if (_visited) return; _visited = true;
+i0.initReflector();
+}
